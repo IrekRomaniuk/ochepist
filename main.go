bc16d6dc8a32d3d4101446c32db10d8e267de846
irom77 <irek.romaniuk@gmail.com>
2017-06-06 13:06:05 -0400

    Executed Templates for ranges are correct

--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 	"bufio"
 	"strings"
 	"os"
+	"text/template"
+	"log"
 )
 
+func init() {
+	
+}
 
 
 func main() {
@@ -17,15 +22,33 @@ if err != nil {
 	os.Exit(1)
 }
 
+tpl, err := template.ParseGlob("templates/*")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+dbedit := map[string]string{"name": "", "ipaddr": "", "ipaddr_first": "", "ipaddr_last": "", "netmask": ""}
+
 scanner := bufio.NewScanner(strings.NewReader(data))
+	
+	
       for scanner.Scan() {
 				//fmt.Println(scanner.Text())
-				line:=scanner.Text()
+				line:=strings.Trim(scanner.Text(),"")
 			  	switch  {
 			  	case strings.ContainsAny(line,"-"):
 				    s:= strings.Split(line,"-")
 					if utils.ValidIP4(s[0]) && utils.ValidIP4(s[1]) {
-						fmt.Println("r" + line, s[0], s[1])
+						//fmt.Println("r" + line, s[0], s[1])
+						dbedit["name"] = "r" + line
+						dbedit["ipaddr_first"] = s[0]
+						dbedit["ipaddr_last"] = s[1]
+						
+						err := tpl.ExecuteTemplate(os.Stdout, "ranges.gotxt", dbedit)
+						if err != nil {
+						log.Fatalln(err)
+						}
+						//fmt.Println("\n",dbedit)
 					}		
 			 	case strings.ContainsAny(line,"/"):
 				 	s:= strings.Split(line,"/")
