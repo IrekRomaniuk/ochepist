124e6223d62234682c4b16c00c2909d4c34a7639
irom77 <irek.romaniuk@gmail.com>
2017-09-06 14:52:29 -0400

    ochepist.sh added

---
+++ ochepist.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+# Source the Check Point profile for library and paths settings
+#
+export `grep "CPDIR_PATH=" /etc/init.d/firewall1`
+[ -f $CPDIR_PATH/tmp/.CPprofile.sh ] || {
+    echo "--- Fatal error: cant find CPprofile.sh !!"
+    # We are unable to setup essential variables
+    echo "Unable to setup needed variables. Stopping." >> $logfile
+    exit 2
+}
+source $CPDIR_PATH/tmp/.CPprofile.sh
+
+# Setup the MDS Environment
+#$MDSDIR/scripts/MDSprofile.sh
+
+mdsenv CMA
+/var/scripts/ochepist/bin/ochepist_lin
+if [ $? -eq 0 ]; then
+    echo "NOT CHANGED" 
+else
+    echo "HAS CHANGED" 
+    dos2unix results/g-ochepist-dbedit.txt
+    dbedit -local -ignore_script_failure -globallock -f results/g-ochepist-dbedit.txt
+fi
+echo $?
+exit
+echo "?!"
+if [ -f /opt/CPmds-R77/customers/CMA/CPsuite-R77/fw1/tmp/manage.lock ] 
+        then 
+                /opt/CPmds-R77/bin/disconnect_client 
+        fi
+#fwm load policy_name.W gateway_name 2>&1
